e525f1683031674c63b30c064e687bee7939143d
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-08-14 02:14:45 +0900

    testdb名quaint-api-testじゃなくてquaint-app-testだった

--- db/initdb/test_db.sql
+++ db/initdb/test_db.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS `quaint-api-test`;
-GRANT ALL ON `quaint-api-test`.* TO 'quaint'@'%';
+CREATE DATABASE `quaint-app-test`;
+GRANT ALL ON `quaint-app-test`.* TO 'docker_dev'@'%';
