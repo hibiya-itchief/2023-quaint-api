b2bc6b840b14fdd0009a2b672899c6d769346c3d
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-09-15 04:12:18 +0900

    ユーザーのログイン時にログを記録

--- app/dep.py
+++ app/dep.py
@@ -71,6 +71,7 @@ def login_for_access_token(username:str,password:str,db:Session):
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=ACCESS_TOKEN_EXPIRE
     )
+    crud.log(db,schemas.LogCreate(timestamp=datetime.now(),user=username,object="/users/me/login [POST]",operation='ユーザーのログイン',result=True))
     return {"access_token": access_token, "token_type": "bearer"}
 
 
