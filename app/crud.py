d0db1a0ab216edf355f3b40f2df2d81546bae360
Ekke <ekke@ekke.jp>
2023-09-12 17:52:37 +0900

    外部キー制約　crud.py を更新

--- app/crud.py
+++ app/crud.py
@@ -241,7 +241,7 @@ def create_ticket(db:Session,event:schemas.Event,user:schemas.JWTUser,person:int
     db.refresh(db_ticket)
     return db_ticket
 def spectest_ticket(db:Session,user:schemas.JWTUser):
-    db_ticket = models.Ticket(id=ulid.new().str,group_id="spectest0426",event_id="01GYEW7B98AXT2HMQNA9STK657",owner_id=user.name,person=1,is_used=False,created_at=datetime.now(timezone(timedelta(hours=+9))).isoformat())
+    db_ticket = models.Ticket(id=ulid.new().str,group_id="testgroup",event_id="01H9PTVFH7CS30A5RKBKMAQ82R",owner_id=user.name,person=1,is_used=False,created_at=datetime.now(timezone(timedelta(hours=+9))).isoformat())
     db.add(db_ticket)
     db.commit()
     db.refresh(db_ticket)
