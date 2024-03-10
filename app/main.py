10ed0d71592ac5cec51feb1036cecddc1e9df876
seigo <utti.nbst.s@gmail.com>
2024-03-10 17:52:08 +0900

    redisからgroupとeventの情報を取得する機能を追加

--- app/main.py
+++ app/main.py
@@ -736,7 +736,24 @@ def update_board_data(permission:schemas.JWTUser=Depends(auth.admin), db:Session
     '/board/groups',
     summary="/boardで使用する団体情報の取得",
     tags=["board"],
-    description="boardで使用する団体情報を一括で取得します。"
+    description="redisから/boardで使用する団体情報を一括で取得します。（最新情報でない可能性があります。呼び出し前に/board/updateを実行することを推薦します。）"
 )
-def get_all_groups(permission:schemas.JWTUser=Depends(auth.admin)):
-    pass
+def get_all_redis_groups():
+    cacheresult=redis_get_if_possible("board_groups")
+    if cacheresult:
+        return json.loads(cacheresult)
+    else:
+        return "データが存在しません"
+    
+@app.get(
+    '/board/groups/{group_id}/events',
+    summary="/boardで使用するイベント情報の取得",
+    tags=["board"],
+    description="redisから/boardで使用する指定された団体idを持つイベント一覧を一括取得します。"
+)
+def get_all_redis_events(group_id):
+    cacheresult=redis_get_if_possible("board_events:"+group_id)
+    if cacheresult:
+        return json.loads(cacheresult)
+    else:
+        return 'データが存在しません'
