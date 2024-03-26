0e3e80c24519a79bfff9d8658854ee81daa2b39a
seigo <utti.nbst.s@gmail.com>
2024-03-26 12:14:43 +0900

    docker_startup.shを正しく実行するように変更

--- docker_startup.sh
+++ docker_startup.sh
@@ -2,8 +2,6 @@
 cd workspace
 python3 -m pip install --upgrade pip
 pip install -r ./requirements.txt
-wait
 chmod +x ./wait-for-it.sh
 bash ./wait-for-it.sh db:3306 -t 60 -- alembic upgrade head
 uvicorn app.main:app --host 0.0.0.0 --reload
-echo done
